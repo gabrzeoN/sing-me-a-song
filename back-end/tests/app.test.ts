0a60ab23a9cc4be332afc6bad0316729fbc6a668
Gabriel Cari <g.cari@hotmail.com>
2022-07-27 12:18:21 -0300

    feat-tests: POST /upvote; /downvote; GET /top.

--- back-end/tests/app.test.ts
+++ back-end/tests/app.test.ts
@@ -5,7 +5,7 @@ import { prisma } from "./../src/database.js";
 import * as scenarioFactory from "./factories/scenarioFactory.js"
 import * as recommendationFactory from "./factories/recommendationFactory.js"
 
-const agent = supertest(app);
+export const agent = supertest(app);
 
 beforeEach(async () => {
     await scenarioFactory.deleteAllData();
@@ -80,5 +80,121 @@ describe("/GET recommendations", ()=> {
         expect(response.statusCode).toEqual(200);
         const recommendationCreated = await prisma.recommendation.findMany();
         expect(recommendationCreated.length).toEqual(5);
+    });
+});
+
+describe("/GET recommendations/:id", ()=> {
+    it("not possible to get recommendation that does not exist, 404",async () => {
+        const response = await agent.get("/recommendations/0")
+        expect(response.statusCode).toEqual(404);
+        const recommendationCreated = await prisma.recommendation.findMany({
+            where: {id: 0}
+        });
+        expect(recommendationCreated.length).toEqual(0);
+    });
+
+    it("is possible to get a recommendation by its id, 200",async () => {
+        const recommendations = await scenarioFactory.fiveRecommendations();
+        const response = await agent.get(`/recommendations/${recommendations[3].id}`);
+        expect(response.statusCode).toEqual(200);
+        const recommendationCreated = await prisma.recommendation.findMany({
+            where: {id: recommendations[3].id}
+        });
+        expect(recommendationCreated.length).toEqual(1);
+    });
+});
+
+describe("/POST recommendations/:id/upvote", ()=> {
+    it("it is not possible to upvote a recommendation that does not exist, 404",async () => {
+        const response = await agent.post("/recommendations/0/upvote")
+        expect(response.statusCode).toEqual(404);
+        const recommendationCreated = await prisma.recommendation.findMany({
+            where: {id: 0}
+        });
+        expect(recommendationCreated.length).toEqual(0);
+    });
+
+    it("it is possible to upvote a recommendation, 200",async () => {
+        const recommendations = await scenarioFactory.fiveRecommendations();
+        const response = await agent.post(`/recommendations/${recommendations[3].id}/upvote`);
+        expect(response.statusCode).toEqual(200);
+        const recommendationCreated = await prisma.recommendation.findMany({
+            where: {id: recommendations[3].id, score: 1}
+        });
+        expect(recommendationCreated.length).toEqual(1);
+    });
+});
+
+describe("/POST recommendations/:id/downvote", ()=> {
+    it("it is not possible to downvote a recommendation that does not exist, 404",async () => {
+        const response = await agent.post("/recommendations/0/downvote")
+        expect(response.statusCode).toEqual(404);
+        const recommendationCreated = await prisma.recommendation.findMany({
+            where: {id: 0}
+        });
+        expect(recommendationCreated.length).toEqual(0);
+    });
+
+    it("it is possible to downvote a recommendation, 200",async () => {
+        const recommendations = await scenarioFactory.fiveRecommendations();
+        const response = await agent.post(`/recommendations/${recommendations[3].id}/downvote`);
+        expect(response.statusCode).toEqual(200);
+        const recommendationCreated = await prisma.recommendation.findMany({
+            where: {id: recommendations[3].id, score: -1}
+        });
+        expect(recommendationCreated.length).toEqual(1);
+    });
+});
+
+describe("/POST recommendations/top/:amount", ()=> {
+    it("only the 2 upvoteds recommendations, 200",async () => {
+        const recommendations = await scenarioFactory.fiveRecommendationsTwoUpvotedOneDownvoted();
+        const response = await agent.get(`/recommendations/top/2`);
+        expect(response.statusCode).toEqual(200);
+        const recommendationCreated = await prisma.recommendation.findMany({
+            orderBy: { score: "desc" },
+            take: 2,
+        });
+        expect(recommendationCreated.length).toEqual(2);
+        expect(recommendationCreated[0].id).toEqual(recommendations.upvoted2x.id);
+        expect(recommendationCreated[1].id).toEqual(recommendations.upvoted1x.id);
+    });
+
+    it("all top recommendations, 200",async () => {
+        const recommendations = await scenarioFactory.fiveRecommendationsTwoUpvotedOneDownvoted();
+        const response = await agent.get(`/recommendations/top/5`);
+        expect(response.statusCode).toEqual(200);
+        const recommendationCreated = await prisma.recommendation.findMany({
+            orderBy: { score: "desc" },
+            take: 5,
+        });
+        expect(recommendationCreated.length).toEqual(5);
+        expect(recommendationCreated[0].id).toEqual(recommendations.upvoted2x.id);
+        expect(recommendationCreated[1].id).toEqual(recommendations.upvoted1x.id);
+        expect([recommendations.nonvoted[0].id, recommendations.nonvoted[1].id]).toContain(recommendationCreated[2].id);
+        expect([recommendations.nonvoted[0].id, recommendations.nonvoted[1].id]).toContain(recommendationCreated[3].id);
+        expect(recommendationCreated[4].id).toEqual(recommendations.downvoted1x.id);
+    });
+
+    it("returns 5 recomendations even if amount is greater than 5, 200",async () => {
+        const recommendations = await scenarioFactory.fiveRecommendationsTwoUpvotedOneDownvoted();
+        const response = await agent.get(`/recommendations/top/7`);
+        expect(response.statusCode).toEqual(200);
+        const recommendationCreated = await prisma.recommendation.findMany({
+            orderBy: { score: "desc" },
+            take: 5,
+        });
+        expect(recommendationCreated.length).toEqual(5);
+        expect(recommendationCreated[0].id).toEqual(recommendations.upvoted2x.id);
+        expect(recommendationCreated[1].id).toEqual(recommendations.upvoted1x.id);
+        expect([recommendations.nonvoted[0].id, recommendations.nonvoted[1].id]).toContain(recommendationCreated[2].id);
+        expect([recommendations.nonvoted[0].id, recommendations.nonvoted[1].id]).toContain(recommendationCreated[3].id);
+        expect(recommendationCreated[4].id).toEqual(recommendations.downvoted1x.id);
+    });
+
+    it("amount is not send through params, 500",async () => {
+        await scenarioFactory.fiveRecommendationsTwoUpvotedOneDownvoted();
+        const response = await agent.get(`/recommendations/top/`);
+        expect(response.statusCode).toEqual(500);
     });
 });
--- back-end/tests/factories/scenarioFactory.ts
+++ back-end/tests/factories/scenarioFactory.ts
@@ -1,5 +1,6 @@
 import { prisma } from "./../../src/database.js";
 import * as recommendationFactory from "./recommendationFactory.js"
+import { agent } from "../app.test.js";
 
 export async function deleteAllData() {
     await prisma.$transaction([
@@ -13,11 +14,26 @@ export async function fiveRecommendations() {
     const recommendation3 = await recommendationFactory.createRecommendation();
     const recommendation4 = await recommendationFactory.createRecommendation();
     const recommendation5 = await recommendationFactory.createRecommendation();
-    return {
+    return [
         recommendation1,
         recommendation2,
         recommendation3,
         recommendation4,
         recommendation5
+    ]
+}
+
+export async function fiveRecommendationsTwoUpvotedOneDownvoted() {
+    const recommendations = await fiveRecommendations();
+    await agent.post(`/recommendations/${recommendations[0].id}/downvote`);
+    await agent.post(`/recommendations/${recommendations[3].id}/upvote`);
+    await agent.post(`/recommendations/${recommendations[3].id}/upvote`);
+    await agent.post(`/recommendations/${recommendations[4].id}/upvote`);
+    
+    return {
+        upvoted1x: recommendations[4],
+        upvoted2x: recommendations[3],
+        downvoted1x: recommendations[0],
+        nonvoted: [recommendations[1], recommendations[2]]
     };
 }
